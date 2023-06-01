e49bbda92ca84651d8e247e0811c8b67c691dd68
Alejandro Gonzalez Hernandez <agonzalezhernan@uoc.edu>
2023-06-01 17:35:11 +0200

    Ajustes realizados en tailwind.config.js. Se extraen algunos estilos para aplicarlos mediante la directiva @apply.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: '-apple-system, "BlinkMacSystemFont", "Segoe UI", "Roboto", "Helvetica Neue", arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
+    },
+    extend: {
+      colors: {
+        morado: "#550dad",
+        naranja: "#e48201",
+      },
+      fontSize: {
+        "var-s": "calc(1.275rem + .3vw)",
+        "var-m": "calc(1.3rem + .6vw)",
+        "var-l": "calc(1.325rem + .9vw)",
+      },
+    },
   },
   plugins: [],
 };
